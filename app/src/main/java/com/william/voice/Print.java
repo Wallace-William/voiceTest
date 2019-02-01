b962f4c34287f79d6d0fc891c02b23985a83f0f7
Wallace-William <williamleewallace97@gmail.com>
2019-01-31 18:55:37 -0600

    init

---
+++ app/src/main/java/com/william/voice/Print.java
@@ -0,0 +1,15 @@
+package com.william.voice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Print extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_print);
+
+    }
+
+
+}
